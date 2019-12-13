0ee4ea28092e1091a27b54125caff2586be832f6
Martin Braquet <martin.braquet@student.uclouvain.be>
2019-12-13 15:35:25 +0100

    Update figs

--- Project/Project.py
+++ Project/Project.py
@@ -1,22 +1,25 @@
 '''
 Questions: 
-- Validation set for feature selection? No, 
-- How to characterize wd and time? OK
-- Normalization, how it works? standard scaler, 
-- MI, how it works?
-- Compute error on training or val set? Divide in K sets and repeat (bootstrap 632)
+- Validation set for feature selection? No
+- How to characterize wd and time? OK, sin/cos
+- Normalization, how it works? standard scaler
+- MI, how it works? Remove random effect
+- Compute error on training or val set? K-fold cross validation (divide in K sets and repeat) or bootstrap 632 (better)
 - Feature extraction: PCA,...
 
 Greed search inside a K fold
 '''
 
+save = 0
+
 import numpy as np
 import math
 import matplotlib
+import tikzplotlib
 import matplotlib.pyplot as plt
 from datetime import datetime
-import seaborn as sns
-from sklearn.feature_selection import mutual_info_regression
+import seaborn as sns; sns.set()
+from sklearn.metrics import mutual_info_score
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
@@ -35,7 +38,7 @@ def load_data():
 
     X_full = Xpd
     X_full = X_full.drop(columns="wd")
-    X_full = X_full.drop(columns="WSPM")
+    #X_full = X_full.drop(columns="WSPM")
     X_full = X_full.drop(columns="wdc")
     X_full = X_full.drop(columns="year")
     X_full = X_full.drop(columns="month")
@@ -72,10 +75,10 @@ def time_to_coord(X1):
     cos_theta_day = np.cos(theta_day)
     sin_theta_day = np.sin(theta_day)
     X1.insert(X1.shape[1], "time", time, True)
-    X1.insert(X1.shape[1], "sin_theta_year", sin_theta_year, True)
-    X1.insert(X1.shape[1], "cos_theta_year", cos_theta_year, True)
-    X1.insert(X1.shape[1], "sin_theta_day", sin_theta_day, True)
-    X1.insert(X1.shape[1], "cos_theta_day", cos_theta_day, True)
+    X1.insert(X1.shape[1], "syear", sin_theta_year, True)
+    X1.insert(X1.shape[1], "cyear", cos_theta_year, True)
+    X1.insert(X1.shape[1], "sday", sin_theta_day, True)
+    X1.insert(X1.shape[1], "cday", cos_theta_day, True)
     return X1
 
 
@@ -116,11 +119,11 @@ def wd_to_coord(X1):
             X1.at[index, 'wdc'] = 2 * np.pi * (15 / 16)
 
     wd = X1.loc[:, ['wdc']].to_numpy()
-    wd_speed = X1.loc[:, ['WSPM']].to_numpy()
-    sin_wd = np.sin(wd) * wd_speed
-    cos_wd = np.cos(wd) * wd_speed
-    X1.insert(X1.shape[1], "sin_wd", sin_wd, True)
-    X1.insert(X1.shape[1], "cos_wd", cos_wd, True)
+    #wd_speed = X1.loc[:, ['WSPM']].to_numpy()
+    sin_wd = np.sin(wd)# * wd_speed
+    cos_wd = np.cos(wd)# * wd_speed
+    X1.insert(13, "swd", sin_wd, True)
+    X1.insert(14, "cwd", cos_wd, True)
     return X1
 
 
@@ -135,8 +138,14 @@ def project_correlation(X, Y, fig=True):
     XY = pd.concat([X, Y], axis=1)
     corr = XY.corr()
 
-    plt.figure(figsize=(25, 15))
-    sns.heatmap(corr, annot=True, cmap=plt.cm.Reds).set_title('Correlation')
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+    with sns.axes_style("white"):
+        sns.heatmap(np.abs(corr), mask=mask, annot=True, cmap=plt.cm.Reds, vmin=0, vmax=1, linewidths=.5).set_title('Correlation')
+
+    if save:
+        tikzplotlib.save("correlation.tex")
+    
     plt.show()
 
     return corr
@@ -153,16 +162,26 @@ def project_mutual_info(X, Y):
     mut = np.zeros((n, n))
     for i in range(n):
         for j in range(n):
-            mut[i, j] = mutual_info_regression(XY[:, i].reshape((XY.shape[0], 1)), XY[:, j],
-                                               random_state=0) if j >= i else mut[j, i]
+            mut[i, j] = mutual_info_score(XY[:, i], XY[:, j]) if j >= i else mut[j, i]
 
+    diag_mut = np.copy(np.diag(mut))
+    for i in range(n):
+        for j in range(n):
+            mut[i, j] = mut[i, j] / math.sqrt(diag_mut[i] * diag_mut[j])
     mut = pd.DataFrame(mut)
     mut.columns = XY_col
     mut.index = mut.columns
 
-    plt.figure(figsize=(25, 15))
-    sns.heatmap(mut, annot=True, cmap=plt.cm.Reds).set_title('Mutual Information')
+    mask = np.zeros_like(mut)
+    mask[np.triu_indices_from(mask)] = True
+    with sns.axes_style("white"):
+        sns.heatmap(np.abs(mut), mask=mask, annot=True, cmap=plt.cm.Reds, vmin=0, vmax=1, linewidths=.5).set_title('Mutual Information')
+    
+    if save:
+        tikzplotlib.save("MI.tex")
+    
     plt.show()
+
 
     return mut
 
@@ -178,7 +197,6 @@ def project_linear_regression(X, Y):
     RMSE = rmse(Y, Y_pred)
     print('Linear regression RMSE :', RMSE)
 
-    #error = bootstrap_point632_score(model, X, Y, method='.632+')
     error = math.sqrt(np.mean(bootstrap_point632_score(model, X, Y)))
     print('Linear regression bootstrap 632 error :', error)
 
@@ -186,48 +204,36 @@ def project_linear_regression(X, Y):
 
 
 # K-nearest neighbours
-def project_KNN(X, Y, n):
+def project_KNN(X, Y, n, method_name='KNN', compute=True):
     print('KNN...')
     
-    def KNN(X, Y, n, method_name, compute):
-        neighbors = np.arange(1, n + 1, 1)
-        if compute:
-            error_knn = np.zeros(n)
-            for i in range(0, n):
-                model = KNeighborsRegressor(n_neighbors=neighbors[i], metric='euclidean')
-                Y_pred = model.fit(X, Y).predict(X)
-                RMSE = rmse(Y, Y_pred)
-                error_knn[i] = math.sqrt(np.mean(bootstrap_point632_score(model, X, Y, n_splits=100)))
-                print(method_name, 'RMSE ( k =', neighbors[i], ') :', RMSE)
-                print(method_name, 'bootstrap 632 error ( k =', neighbors[i], ') :', error_knn[i])
+    neighbors = np.arange(1, n + 1, 1)
+    if compute:
+        error_knn = np.zeros(n)
+        for i in range(0, n):
+            model = KNeighborsRegressor(n_neighbors=neighbors[i], metric='euclidean')
+            Y_pred = model.fit(X, Y).predict(X)
+            RMSE = rmse(Y, Y_pred)
+            error_knn[i] = math.sqrt(np.mean(bootstrap_point632_score(model, X, Y, n_splits=100)))
+            print(method_name, 'RMSE ( k =', neighbors[i], ') :', RMSE)
+            print(method_name, 'bootstrap 632 error ( k =', neighbors[i], ') :', error_knn[i])
+    else:
+        if method_name == 'KNN':
+            error_knn = np.array([44.63007435, 42.91722747, 42.42618258, 42.39780082, 42.54022997, 42.59120806, 42.71413344, 42.92639601, 42.86850688, 43.09182013, 43.36117329, 43.41121083, 43.65708279, 43.69896532, 43.86472366, 43.78765416, 44.00862374, 44.30227774, 44.29008714, 44.46076153, 44.53703636, 44.63881345, 44.69277326, 44.75460404, 44.81400926, 44.9321936, 45.14968511, 44.91520762, 45.18015732, 45.08772065, 45.30150145, 45.21632641, 45.3992413, 45.54305763, 45.44595713, 45.67029185, 45.71647618, 45.55042106, 45.79218583, 45.80604866, 45.89558866, 45.95351281, 45.95303281, 46.01579606, 46.04745272, 46.09775259, 46.17958316, 46.28358257, 46.26775877, 46.40855521])
         else:
-            if method_name == 'KNN':
-                error_knn = np.array([44.63007435, 42.91722747, 42.42618258, 42.39780082, 42.54022997, 42.59120806, 42.71413344, 42.92639601, 42.86850688, 43.09182013, 43.36117329, 43.41121083, 43.65708279, 43.69896532, 43.86472366, 43.78765416, 44.00862374, 44.30227774, 44.29008714, 44.46076153, 44.53703636, 44.63881345, 44.69277326, 44.75460404, 44.81400926, 44.9321936, 45.14968511, 44.91520762, 45.18015732, 45.08772065, 45.30150145, 45.21632641, 45.3992413, 45.54305763, 45.44595713, 45.67029185, 45.71647618, 45.55042106, 45.79218583, 45.80604866, 45.89558866, 45.95351281, 45.95303281, 46.01579606, 46.04745272, 46.09775259, 46.17958316, 46.28358257, 46.26775877, 46.40855521])
-            else:
-                error_knn = np.array([52.73763501, 50.26260653, 49.43199863, 49.02281048, 48.68253806, 48.85401044, 48.67349653, 48.72226453, 48.57146228, 48.50207035, 48.56443761, 48.51289697, 48.53336497, 48.50314503, 48.47753564, 48.47326746, 48.57352092, 48.5564091, 48.4619637, 48.53501412, 48.50419066, 48.56703669, 48.5808846, 48.60146488, 48.61727791, 48.61611907, 48.75758009, 48.71311524, 48.77881461, 48.77234174, 48.8188231, 48.78412026, 48.7703691, 48.80950418, 48.83629899, 48.88289063, 48.93794985, 48.96546474, 49.01236155, 49.07860634, 48.99108467, 49.00323927, 48.94704306, 48.8915739, 49.09580907, 49.06468691, 49.03424426, 49.22457642, 49.13059562, 49.23870995])
-        plt.plot(neighbors, error_knn, 'b')
-        plt.xlabel('neighbours')
-        plt.ylabel('Error' + method_name)
+            error_knn = np.array([52.73763501, 50.26260653, 49.43199863, 49.02281048, 48.68253806, 48.85401044, 48.67349653, 48.72226453, 48.57146228, 48.50207035, 48.56443761, 48.51289697, 48.53336497, 48.50314503, 48.47753564, 48.47326746, 48.57352092, 48.5564091, 48.4619637, 48.53501412, 48.50419066, 48.56703669, 48.5808846, 48.60146488, 48.61727791, 48.61611907, 48.75758009, 48.71311524, 48.77881461, 48.77234174, 48.8188231, 48.78412026, 48.7703691, 48.80950418, 48.83629899, 48.88289063, 48.93794985, 48.96546474, 49.01236155, 49.07860634, 48.99108467, 49.00323927, 48.94704306, 48.8915739, 49.09580907, 49.06468691, 49.03424426, 49.22457642, 49.13059562, 49.23870995])
+    plt.plot(neighbors, error_knn, 'b')
+    plt.xlabel('neighbours')
+    plt.ylabel('Error' + method_name)
+
+    if save:
+        tikzplotlib.save("KNN.tex")
     
-        plt.show(block=False)
-        
-        return error_knn
-        
-    # KNN
-    error_knn = KNN(X, Y, n, method_name='KNN', compute=False)
-    
-    # KNN with PCA
-    pca = PCA(n_components=4)
-    X_PCA = pca.fit_transform(X)
-    print(pca.explained_variance_ratio_)
-    error_knn_PCA = KNN(X_PCA, Y, n, method_name='KNN PCA', compute=False)
+    plt.show(block=False)
 
-    # plot_file = "./knn_mse.eps"
-    # fig.savefig(plot_file, facecolor='w', edgecolor='w', format='eps', bbox_inches='tight', pad_inches=0)
+    return error_knn
 
-    return error_knn, error_knn_PCA
-
-# Lasso
+# Lasso (no feature selection)
 def project_lasso(X, Y, n):
     print('Lasso...')
     
@@ -250,6 +256,10 @@ def project_lasso(X, Y, n):
     plt.xlabel('alpha')
     plt.ylabel('Error Lasso')
     plt.xscale('log')
+
+    if save:
+        tikzplotlib.save("Lasso.tex")
+    
     plt.show(block=False)
     
     return error_lasso
@@ -266,9 +276,19 @@ nd, nf = X_full.shape  # nd: nbr of data, nf: nbr of features
 
 ### Features selection
 
-#corr = project_correlation(X_full, Y)
+if 'corr' not in locals():
+    corr = project_correlation(X_full, Y)
 
-#mut = project_mutual_info(X_full, Y)
+if 'mut' not in locals():
+    mut = project_mutual_info(X_full, Y)
+
+if 'index' not in locals():
+    index = mut.values[-1,:-1] > 0.2
+    index[4] = False
+    index[5] = False
+    index[12] = False
+    X_select = X_full.values[:,index]
+
 
 '''
 Keeps only the relevant features
@@ -289,16 +309,26 @@ Keeps only the relevant features
           13: wd_2 (second wind coordinate)
 '''
 
-X = X_full.values#[:, mut.values[:-1, -1] > 0.1]
+### Features extraction
 
+# PCA
+pca = PCA(n_components=4)
+X_PCA = pca.fit_transform(X_full.values)
+print(pca.explained_variance_ratio_)
+
+
+### Algorithms test
+
+X = X_select
 
 error_lin_reg = project_linear_regression(X, Y.values)
 
 print(error_lin_reg)
 
-n = 50
+n = 5
 
-error_knn, error_knn_PCA = project_KNN(X, Y.values, n)
+error_knn = project_KNN(X, Y.values, n)
+error_knn_PCA = project_KNN(X_PCA, Y.values, n)
 
 print(error_knn)
 print(error_knn_PCA)
